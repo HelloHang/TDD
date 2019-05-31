513cdd7e17c4fa66640497471956725aa3d9bf1d
dangao <dangao@Objectivasoftware.com>
2019-05-31 17:51:43 +0800

    Update V3

--- src/main/java/dan/gao/v3/Parser.java
+++ src/main/java/dan/gao/v3/Parser.java
@@ -1,48 +1,151 @@
 package dan.gao.v3;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
+
 
 /**
  * @author: Daniels Gao
  * @date: 2019/5/30 22:56
  */
-public class Parser {
+public class Parser
+{
+	public static final String TYPE_STRING = "String";
+	public static final String TYPE_INT = "int";
+	public static final String BOOLEAN = "boolean";
+	public static final String PREFIX_PARAM = "-";
+	private HashMap<String, Object> result;
 
+	public void parse(String[] input, Schema schema)
+	{
+		result = new HashMap<>();
+		schema.getDefineModel().forEach(this::initResult);
+		setParamValueBy(input, schema);
+	}
 
-  private HashMap<String, Object> result;
+	private void initResult(String key, String value)
+	{
+		if (isBoolean(value))
+		{
+			result.put(key, Boolean.FALSE);
+		}
+		if (isIntType(value))
+		{
+			result.put(key, 0);
+		}
+		if (isStringType(value))
+		{
+			result.put(key, StringUtils.EMPTY);
+		}
+	}
 
-  public void parse(String[] input, Schema schema) {
-    result = new HashMap<>();
-    schema.getDefineModel().forEach((key, value) -> {
-      if ("boolean".equals(value)) {
-        result.put(key, Boolean.FALSE);
-      }
-      if ("int".equals(value)) {
-        result.put(key, 0);
-      }
-      if ("String".equals(value)) {
-        result.put(key, "");
-      }
-    });
+	private void setParamValueBy(String[] input, Schema schema)
+	{
+		for (int i = 0; i < input.length; i++)
+		{
+			if (isParamKey(input[i]))
+			{
+				resolveParamKey(input, schema, i);
+			}
 
-    for (int i = 0; i < input.length; i++) {
-      if (input[i].startsWith("-")) {
-        String type = schema.getType(input[i].substring(1));
-        if ("boolean".equals(type)) {
-          result.put(input[i].substring(1), Boolean.TRUE);
-        }
-        if ("int".equals(type)) {
-          result.put(input[i].substring(1), Integer.valueOf(input[i + 1]));
-        }
-        if ("String".equals(type)) {
-          result.put(input[i].substring(1), input[i + 1]);
-        }
-      }
-    }
+			if (isFirstParam(i) && !isParamKey(input[i]))
+			{
+				throw new IllegalArgumentException("Please input a valid param.");
+			}
 
-  }
+			if (!isFirstParam(i) && !isParamKey(input[i - 1]))
+			{
+				throw new IllegalArgumentException("Please input a valid param");
+			}
+		}
+	}
 
-  public Object getValue(String param) {
-    return result.get(param);
-  }
+	private void resolveParamKey(String[] input, Schema schema, int i)
+	{
+		String type = schema.getType(getParamKey(input[i]));
+		if (isBoolean(type))
+		{
+			resolveBooleanParam(input, i);
+		}
+		if (isIntType(type))
+		{
+			resolveIntParam(input, i);
+		}
+		if (isStringType(type))
+		{
+			resolveStringParam(input, i);
+		}
+		if (type == null)
+		{
+			throw new IllegalArgumentException("Invalid param");
+		}
+	}
+
+	private void resolveStringParam(String[] input, int i)
+	{
+		if (!hasNext(input, i) || isParamKey(input[i + 1]))
+		{
+			throw new IllegalArgumentException("Please input a valid value for param[d]");
+		}
+		result.put(getParamKey(input[i]), input[i + 1]);
+	}
+
+	private void resolveIntParam(String[] input, int i)
+	{
+		if (!hasNext(input, i) || !StringUtils.isNumeric(input[i + 1]))
+		{
+			throw new IllegalArgumentException("Please input a valid value for param[p]");
+		}
+		result.put(getParamKey(input[i]), Integer.valueOf(input[i + 1]));
+	}
+
+	private void resolveBooleanParam(String[] input, int i)
+	{
+		if (hasNext(input, i) && !isParamKey(input[i + 1]))
+		{
+			throw new IllegalArgumentException(String.format("Invalid value[%s]", input[i + 1]));
+		}
+		result.put(getParamKey(input[i]), Boolean.TRUE);
+	}
+
+	private boolean isFirstParam(int i)
+	{
+		return i - 1 < 0;
+	}
+
+	private String getParamKey(String s)
+	{
+		return s.substring(1);
+	}
+
+	private boolean hasNext(String[] input, int i)
+	{
+		return i + 1 < input.length;
+	}
+
+	private boolean isParamKey(String str)
+	{
+		return StringUtils.startsWith(str, PREFIX_PARAM);
+	}
+
+	private boolean isStringType(String value)
+	{
+		return TYPE_STRING.equals(value);
+	}
+
+	private boolean isIntType(String value)
+	{
+		return TYPE_INT.equals(value);
+	}
+
+	private boolean isBoolean(String value)
+	{
+		return BOOLEAN.equals(value);
+	}
+
+	public Object getValue(String param)
+	{
+		return result.get(param);
+	}
 }
--- src/test/java/dan/gao/v3/ParserTest.java
+++ src/test/java/dan/gao/v3/ParserTest.java
@@ -3,60 +3,142 @@ package dan.gao.v3;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-
-public class ParserTest {
-
-  private Parser parser;
-  private Schema schema;
-
-  @Before
-  public void init() {
-    parser = new Parser();
-    schema = new Schema(new String[]{"l:boolean","p:int","d:String"});
-  }
-
-  @Test
-  public void test(){
-    String[] input = {"l:boolean","p:int","d:String"};
-    Schema schema = new Schema(input);
-    assertEquals(schema.getType("l"),"boolean");
-    assertEquals(schema.getType("p"),"int");
-    assertEquals(schema.getType("d"),"String");
-    assertTrue(schema.isValidParam("l"));
-    assertFalse(schema.isValidParam("q"));
-  }
-
-  @Test
-  public void testParse_EmptyInput(){
-    String[] input = {};
-    parser.parse(input, schema);
-    assertEquals(parser.getValue("l"), Boolean.FALSE);
-    assertEquals(parser.getValue("p"), 0);
-    assertEquals(parser.getValue("d"),"");
-  }
-
-  @Test
-  public void testParse_SingleParam_boolean(){
-    String[] input = {"-l"};
-    parser.parse(input,schema);
-    assertEquals(parser.getValue("l"), Boolean.TRUE);
-  }
-
-  @Test
-  public void testSingle_int(){
-    String[] input = {"-p", "8080"};
-    parser.parse(input, schema);
-    assertEquals(parser.getValue("p"), 8080);
-  }
-
-  @Test
-  public void testSingle_String(){
-    String[] input = {"-d", "/var"};
-    parser.parse(input, schema);
-    assertEquals(parser.getValue("d"), "/var");
-  }
+import org.junit.rules.ExpectedException;
 
 
+public class ParserTest
+{
 
+	private Parser parser;
+	private Schema schema;
+
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	@Before
+	public void init()
+	{
+		parser = new Parser();
+		schema = new Schema(new String[] { "l:boolean", "p:int", "d:String" });
+	}
+
+	@Test
+	public void test()
+	{
+		String[] input = { "l:boolean", "p:int", "d:String" };
+		Schema schema = new Schema(input);
+		assertEquals(schema.getType("l"), "boolean");
+		assertEquals(schema.getType("p"), "int");
+		assertEquals(schema.getType("d"), "String");
+		assertTrue(schema.isValidParam("l"));
+		assertFalse(schema.isValidParam("q"));
+	}
+
+	@Test
+	public void testParse_EmptyInput()
+	{
+		String[] input = {};
+		parser.parse(input, schema);
+		assertEquals(parser.getValue("l"), Boolean.FALSE);
+		assertEquals(parser.getValue("p"), 0);
+		assertEquals(parser.getValue("d"), "");
+	}
+
+	@Test
+	public void testParse_SingleParam_boolean()
+	{
+		String[] input = { "-l" };
+		parser.parse(input, schema);
+		assertEquals(parser.getValue("l"), Boolean.TRUE);
+	}
+
+	@Test
+	public void testSingle_int()
+	{
+		String[] input = { "-p", "8080" };
+		parser.parse(input, schema);
+		assertEquals(parser.getValue("p"), 8080);
+	}
+
+	@Test
+	public void testSingle_String()
+	{
+		String[] input = { "-d", "/var" };
+		parser.parse(input, schema);
+		assertEquals(parser.getValue("d"), "/var");
+	}
+
+	@Test
+	public void testParse_SingleParam_int()
+	{
+		String[] input = { "-p" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Please input a valid value for param[p]");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void testParse_SingleParam_String()
+	{
+		String[] input = { "-d" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Please input a valid value for param[d]");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void testParse_InvalidInt()
+	{
+		String[] input = { "-l", "-p", "-d", "/var" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Please input a valid value for param[p]");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void test_Parse_InvalidBoolean()
+	{
+		String[] input = { "-l", "12331" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Invalid value[12331]");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void testParse_InvalidString()
+	{
+		String[] input = { "-l", "-d", "-p", "8080" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Please input a valid value for param[d]");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void testParse_InvalidParam()
+	{
+		String[] input = { "8080" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Please input a valid param.");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void testParse_InvalidParamNoKey()
+	{
+		String[] input = { "-d", "/var", "p" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Please input a valid param");
+		parser.parse(input, schema);
+	}
+
+	@Test
+	public void testParse_InvalidParamKay(){
+		String[] input = { "-w", "ss" };
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage("Invalid param");
+		parser.parse(input,schema);
+	}
 }
